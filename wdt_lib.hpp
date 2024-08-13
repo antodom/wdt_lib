718f48bb83319a624295e5ae1882dddc5ef0809e
Antonio C. Domínguez-Brito <antonio.dominguez@ulpgc.es>
2024-08-13 12:13:50 +0100

    licenses added to the project

--- wdt_lib.hpp
+++ wdt_lib.hpp
@@ -1,3 +1,28 @@
+/**
+ ** wdt_lib library
+ ** Copyright (C) 2024
+ **
+ **   Antonio C. Domínguez Brito <antonio.dominguez@ulpgc.es>
+ **     División de Robótica y Oceanografía Computacional <www.roc.siani.es>
+ **     and Departamento de Informática y Sistemas <www.dis.ulpgc.es>
+ **     Universidad de Las Palmas de Gran  Canaria (ULPGC) <www.ulpgc.es>
+ **  
+ ** This file is part of the wdt_lib library.
+ ** The wdt_lib library is free software: you can redistribute it and/or modify
+ ** it under  the  terms of  the GNU  General  Public  License  as  published  by
+ ** the  Free Software Foundation, either  version  3  of  the  License,  or  any
+ ** later version.
+ ** 
+ ** The  wdt_lib library is distributed in the hope that  it  will  be  useful,
+ ** but   WITHOUT   ANY WARRANTY;   without   even   the  implied   warranty   of
+ ** MERCHANTABILITY or FITNESS FOR A PARTICULAR  PURPOSE.  See  the  GNU  General
+ ** Public License for more details.
+ ** 
+ ** You should have received a copy  (COPYING file) of  the  GNU  General  Public
+ ** License along with the wdt_lib library.
+ ** If not, see: <http://www.gnu.org/licenses/>.
+ **/
+
 /* wdt_lib.hpp
  *
  * author: Antonio C. Domínguez Brito <adominguez@iusiani.ulpgc.es>
